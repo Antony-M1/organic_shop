67cbc9745cf322b0ddf524c6305045c0909d3ca9
manikandanmohan10 <manikandan.mohan@softsuave.com>
2023-10-12 13:47:34 +0000

    shopping cart settings commented

--- organic_shop/organic_cart.py
+++ organic_shop/organic_cart.py
@@ -8,6 +8,7 @@ import frappe.defaults
 from frappe.utils import cint, flt, get_fullname, cstr
 from frappe.contacts.doctype.address.address import get_address_display
 from erpnext.shopping_cart.doctype.shopping_cart_settings.shopping_cart_settings import get_shopping_cart_settings
+
 from frappe.utils.nestedset import get_root_of
 from erpnext.accounts.utils import get_account_name
 from erpnext.utilities.product import get_qty_in_stock
