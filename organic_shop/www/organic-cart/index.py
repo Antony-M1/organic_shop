11b19cccd663e87d72698d04366089a0930c0ad6
manikandanmohan10 <manikandan.mohan@softsuave.com>
2023-10-14 10:47:43 +0000

    get_cart_quotation updated

--- organic_shop/www/organic-cart/index.py
+++ organic_shop/www/organic-cart/index.py
@@ -1,6 +1,7 @@
 import frappe
 import json
-from erpnext.shopping_cart.cart import get_cart_quotation
+# from erpnext.shopping_cart.cart import get_cart_quotation
+from erpnext.e_commerce.shopping_cart.cart import get_cart_quotation
 
 # def get_context(context):
 # 	context.update(get_cart_quotation())
