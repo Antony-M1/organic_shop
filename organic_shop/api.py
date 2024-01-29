885ff7e69168af4ef44f66440fb84312081f9a72
manikandanmohan10 <manikandan.mohan@softsuave.com>
2024-01-29 16:39:23 +0000

    Site creation

--- organic_shop/api.py
+++ organic_shop/api.py
@@ -1,5 +1,11 @@
 import frappe
 import json
+from frappe.website.page_renderers.error_page import ErrorPage
+from frappe.website.page_renderers.not_found_page import NotFoundPage
+from frappe.website.page_renderers.not_permitted_page import NotPermittedPage
+from frappe.website.page_renderers.redirect_page import RedirectPage
+from frappe.website.path_resolver import PathResolver
+
 
 @frappe.whitelist(allow_guest = True)
 def get_list_custom(doctype, fields=None, filters=None, order_by=None,
@@ -14,3 +20,41 @@ def get_list_custom(doctype, fields=None, filters=None, order_by=None,
 	:param limit_page_length: Number of records to be returned (default 20)'''
 	return frappe.db.get_all(doctype, fields=fields, filters=filters, order_by=order_by,
 		limit_start=limit_start, limit_page_length=limit_page_length, ignore_permissions=True)
+
+
+@frappe.whitelist(allow_guest=True)
+def get_customer_reviews():
+	return frappe.get_all('Customer Review',  filters={
+        'show_in_website': 1
+    }, fields=['customer_name', 'content', 'rating', 'customer_image'])
+
+
+def get_response_wishlist(path=None, http_status_code=200):
+	"""Resolves path and renders page"""
+	response = None
+	path = path or frappe.local.request.path
+
+	endpoint = path
+
+	try:
+		path_resolver = PathResolver(path)
+		endpoint, renderer_instance = path_resolver.resolve()
+		response = renderer_instance.render()
+	except frappe.Redirect:
+		return RedirectPage(endpoint or path, http_status_code).render()
+	except frappe.PermissionError as e:
+		response = NotPermittedPage(endpoint, http_status_code, exception=e).render()
+	except frappe.PageDoesNotExistError:
+		response = NotFoundPage(endpoint, http_status_code).render()
+	except Exception as e:
+		response = ErrorPage(exception=e).render()
+
+	return response
+
+
+@frappe.whitelist(allow_guest=True)
+def wishlist_content():
+	path="/wishlist"
+	http_status_code=200
+	response = get_response_wishlist(path, http_status_code)
+	return str(response.data, "utf-8")
